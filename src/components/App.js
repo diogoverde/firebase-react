9ce21bac5fbf067ab736af5187d7662317078785
Diogo <diogo.verde05@gmail.com>
2021-01-07 11:02:54 +0000

    adding router to signup and dahsboard components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
+import React from "react";
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
-import Signup from "./Signup";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard}></Route>
+              <Route path="/signup" component={Signup}></Route>
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
